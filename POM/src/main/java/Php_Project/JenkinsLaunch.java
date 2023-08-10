d219a6ef06b726393583132746a1cea212246c21
Manoj Karthick <manojkarthick.ece@gmail.com>
2023-08-10 15:20:34 +0530

    added tests

---
+++ POM/src/main/java/Php_Project/JenkinsLaunch.java
@@ -0,0 +1,20 @@
+package Php_Project;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class JenkinsLaunch {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://google.com");
+		Thread.sleep(3000);
+		driver.manage().window().maximize();
+	}
+
+}
